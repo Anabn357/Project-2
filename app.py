612994acf8b4668cbc04ecb83321472d0f9af748
Avithegitty81 <64101146+Avithegitty81@users.noreply.github.com>
2020-10-14 17:37:13 -0500

    data.html

--- app.py
+++ app.py
@@ -1,45 +1,28 @@
-from flask import Flask, jsonify
-import pymongo
+from flask import Flask, request
 import json
 import pandas as pd
 
-# setup mongo connection
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+//Create instance for flask app:
 
-# connect to mongo db and collection
-db = client.project_2
+app = Flask(__name__)
 
-# reading data from mongodb database
-master_data = db.master.find_one()
-# sale_data = db.sale.find_one()
-# rating_sale_data = db.rating_sale.find_one()
-# whatoplay_data = db.whatoplay.find_one()
-
-
-# Create an instance of Flask
-app = Flask(__name__)   
- 
+// Create route that renders html and javascript for each page:
 @app.route("/")
-def master():   
-    data = json.loads(master_data)
-    # dc = data.to_dict()
-    # print(data)
-    return "hello"
+def page1():
+    return(index.html, map.js)
 
+@app.route("/")
+def page2():
+    return(per_year.html, per_year.js)
 
+@app.route("/")
+def page3():
+    return(over_year.html, overyear1.js, overyear2.js)   
 
-# # @app.route("/api/kaggle")
-# # def kaggle():   
-    
-# #     kaggle_results = master_table.find_one()
-# #     return jsonify(kaggle_results)
+@app.route("/")
+def page4():
+    return(correlation.html, correlations.js)
 
-# # @app.route("/api/whtoplay")
-# # def kaggle():   
-    
-# #     whatoplay_results = master_table.find_one()
-# #     return jsonify(whatoplay_results)
 
 if __name__ == "__main__":
-    app.run(debug = False)
+    app.run(debug = True)
